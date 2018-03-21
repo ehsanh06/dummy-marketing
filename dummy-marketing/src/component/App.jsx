f59ce3339354d5cceebeecd33ff227fff7bc3de2
Unknown <ehsanhussain991@gmail.com>
2018-03-21 16:16:19 +0000

    added Features component

--- dummy-marketing/src/component/App.jsx
+++ dummy-marketing/src/component/App.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 
 import Header from './Header';
-import Design from './Design'
+import Design from './Design';
+import Features from './Features';
 
 export default class App extends Component {
 
@@ -10,6 +11,7 @@ export default class App extends Component {
       <div className="App">
         <Header />
         <Design />
+        <Features />
       </div>
     );
   }
---
+++ dummy-marketing/src/component/Features.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+export default class Features extends Component {
+
+    render() {
+        return (
+            <section className="features">
+                <div>
+                    <h4>New features</h4>
+                    <h3>Some awesome features</h3>
+                </div>
+                <div>
+                    <div>
+                        <img src="" alt="postcard"/>
+                        <h3>Some awesome features</h3>
+                        <p>
+                            Lorem Ipsum has been the industry's standard 
+                            dummy text ever since the 1500s, when an unknown printer
+                            took a galley of type and scrambled it to make a type specimen book.
+                        </p>
+                    </div>
+                    <div>
+                        <img src="" alt="postcard"/>
+                        <h3>Some awesome features</h3>
+                        <p>
+                            Lorem Ipsum has been the industry's standard 
+                            dummy text ever since the 1500s, when an unknown printer
+                            took a galley of type and scrambled it to make a type specimen book.
+                        </p>
+                    </div>
+                    <div>
+                        <img src="" alt="postcard"/>
+                        <h3>Some awesome features</h3>
+                        <p>
+                            Lorem Ipsum has been the industry's standard 
+                            dummy text ever since the 1500s, when an unknown printer
+                            took a galley of type and scrambled it to make a type specimen book.
+                        </p>
+                    </div>
+
+                </div>
+            </section>
+        );
+    }
+}
