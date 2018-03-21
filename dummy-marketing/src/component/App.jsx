d77c1d0f7cd96b2965e19545907c98e1ab1538c7
Unknown <ehsanhussain991@gmail.com>
2018-03-21 15:46:54 +0000

    Added Header component

--- dummy-marketing/src/component/App.jsx
+++ dummy-marketing/src/component/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import Header from './Header';
+
 export default class App extends Component {
 
   render() {
     return (
       <div className="App">
-        HELLO
+        <Header />        
       </div>
     );
   }
