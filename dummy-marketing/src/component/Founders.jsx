0053ee98d3e2502ceb78873dfbdbd73831375fc7
Unknown <ehsanhussain991@gmail.com>
2018-03-26 21:38:05 +0100

    basic desktop styling complete

--- dummy-marketing/src/component/Founders.jsx
+++ dummy-marketing/src/component/Founders.jsx
@@ -6,30 +6,30 @@ export default class Responsive extends Component {
         return (
             <section className="founders">
                 <div>
-                    <div>
-                        <img src="" alt="avatar"/>
-                        <span>Jon Doe</span>
-                        <small>Co Founder</small>
+                    <div className="founders-profile">
+                        <div className="avatar"></div>
+                        <h3>Jon Doe</h3>
+                        <h4>Co Founder</h4>
                         <p class="testimonial">
                             Lorem Ipsum has been the industry's standard 
                             dummy text ever since the 1500s, when an unknown printer
                             took a galley of type and scrambled it to make a type specimen book.
                         </p>
                     </div>
-                    <div>
-                        <img src="" alt="avatar"/>
-                        <span>Jon Doe</span>
-                        <small>Co Founder</small>
+                    <div className="founders-profile">
+                        <div className="avatar"></div>
+                        <h3>Jon Doe</h3>
+                        <h4>Co Founder</h4>
                         <p class="testimonial">
                             Lorem Ipsum has been the industry's standard 
                             dummy text ever since the 1500s, when an unknown printer
                             took a galley of type and scrambled it to make a type specimen book.
                         </p>
                     </div>
-                    <div>
-                        <img src="" alt="avatar"/>
-                        <span>Jon Doe</span>
-                        <small>Co Founder</small>
+                    <div className="founders-profile">
+                        <div className="avatar"></div>
+                        <h3>Jon Doe</h3>
+                        <h4>Co Founder</h4>
                         <p class="testimonial">
                             Lorem Ipsum has been the industry's standard 
                             dummy text ever since the 1500s, when an unknown printer
@@ -40,22 +40,19 @@ export default class Responsive extends Component {
                 <div>
                     <ul class="brands">
                         <li>
-                            <img src="" alt="brand-logo"/>
+                            <img src="/assets/images/section5-logo1.png" alt="brand-logo"/>
                         </li>
                         <li>
-                            <img src="" alt="brand-logo"/>
+                            <img src="/assets/images/section5-logo2.png" alt="brand-logo"/>
                         </li>
                         <li>
-                            <img src="" alt="brand-logo"/>
+                            <img src="/assets/images/section5-logo3.png" alt="brand-logo"/>
                         </li>
                         <li>
-                            <img src="" alt="brand-logo"/>
+                            <img src="/assets/images/section5-logo4.png" alt="brand-logo"/>
                         </li>
                         <li>
-                            <img src="" alt="brand-logo"/>
-                        </li>
-                        <li>
-                            <img src="" alt="brand-logo"/>
+                            <img src="/assets/images/section5-logo5.png" alt="brand-logo"/>
                         </li>
                     </ul>
                 </div>
