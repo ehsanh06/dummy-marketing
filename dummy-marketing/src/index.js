7e0c1f08be977ddf859adc31646e2e1b3571b313
Unknown <ehsanhussain991@gmail.com>
2018-05-21 21:37:37 +0100

    Added Router

--- dummy-marketing/src/index.js
+++ dummy-marketing/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {BrowserRouter as Router} from 'react -router-dom';
 import App from './component/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render( <Router basename={process.env.PUBLIC_URL}><App /></Router>,
+    document.getElementById('root'));
