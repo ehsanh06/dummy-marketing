932727ec294e106c8fd5bf67717540ffe133d04d
Unknown <ehsanhussain991@gmail.com>
2018-05-27 19:16:34 +0100

    installed React router DOM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react -router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import App from './component/App';
 
 ReactDOM.render( <Router basename={process.env.PUBLIC_URL}><App /></Router>,
