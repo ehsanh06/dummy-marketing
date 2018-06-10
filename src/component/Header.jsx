9f2be579be0dbf1471845a4d26b815315f870371
Unknown <ehsanhussain991@gmail.com>
2018-06-10 10:58:45 +0100

    removed play-button due to it being very unnecessary

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,17 +5,13 @@ export default class Header extends Component {
     scrollToElement = () => {
             let elmnt = document.getElementById("trial");
             elmnt.scrollIntoView({ behavior: 'smooth' });
-    }
-    
+    }    
 
     render() {
         return (
             <header className="landing">
                 <div id="heading-container">
                     <h1>There is no other platforms for you as like</h1>
-                </div>
-                <div id="play-button-container">
-                    <img src="./assets/images/header-icon-play.png" alt="Play"/>
                 </div>
                 <div>
                     <button type="button" onClick={this.scrollToElement.bind(this)}>Try Now</button>
